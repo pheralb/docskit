0855350d76c3657e56722ca6253d44be6e5f8e94
pheralb <pablohdez4340@gmail.com>
2022-12-11 15:47:26 +0000

    ⚒️ Initial app config.

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -16,7 +16,5 @@ function hydrate() {
 if (window.requestIdleCallback) {
   window.requestIdleCallback(hydrate);
 } else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
   window.setTimeout(hydrate, 1);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -27,6 +27,6 @@ export default function Index() {
           </a>
         </li>
       </ul>
-    </div>
+    </>
   );
 }
