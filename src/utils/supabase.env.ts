d50b105defaa388c490a9a85ef52ffacf6a5dd77
pheralb <pablohdez4340@gmail.com>
2022-12-13 20:40:07 +0000

    ⚒️ Update supabase env file.

--- src/utils/supabase.env.ts
+++ src/utils/supabase.env.ts
@@ -1,4 +1,4 @@
 export const supabaseEnv = {
-  SUPABASE_URL: process.env.SUPABASE_URL!,
-  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
+  SUPABASE_URL: process.env.SUPABASE_URL || 'Your Supabase URL',
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || 'Your Supabase Anon Key',
 };
