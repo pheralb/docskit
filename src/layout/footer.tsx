927b4cf719879a3a7018ff0edaf8f09d2e8fabc4
pheralb <pablohdez4340@gmail.com>
2022-12-18 00:01:15 +0000

    ⚒️ Update header & footer layout.

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full py-4 pl-4 pr-6 text-gray-500">
       <div className="flex justify-between">
-        <p className="font-medium">🛫 beta</p>
+        <p className="font-medium">🚧 alpha</p>
         <p className="font-medium">💖 Created by pheralb</p>
       </div>
     </div>
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,50 +1,27 @@
 import Logo from "@/components/icons/logo";
-import { MaybeSession, TypedSupabaseClient } from "@/types/supabase";
 import Button from "@/ui/button";
 import { Link } from "@remix-run/react";
+import { BiLinkExternal } from "react-icons/bi";
 import { IoLogoGithub, IoLogoTwitter } from "react-icons/io5";
 
-const Header = ({
-  supabase,
-  session,
-}: {
-  supabase: TypedSupabaseClient;
-  session: MaybeSession;
-}) => {
-  // Sign in with Github =>
-  const handleGitHubLogin = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-
-    if (error) {
-      console.log({ error });
-    }
-  };
-
+const Header = () => {
   return (
-    <div className="sticky top-0 z-50 w-full py-3 pl-4 pr-6 bg-midnight">
+    <div className="sticky top-0 z-50 w-full py-5 pl-4 pr-6 bg-midnight">
       <div className="flex items-center justify-between">
         <Link to="/">
           <div className="flex items-center space-x-3 text-gray-300 transition-colors duration-150 cursor-pointer hover:text-white">
             <Logo className="w-8 border rounded-full border-neutral-800" />
-            <p className="font-medium">
-              {session ? (
-                <p>{session.user.user_metadata.name}</p>
-              ) : (
-                <p>docskit</p>
-              )}
-            </p>
+            <p className="font-medium">docskit</p>
           </div>
         </Link>
         <div className="flex items-center">
-          <div className="pr-4 ">
-            {session ? (
-              <Button onClick={() => supabase.auth.signOut()}>Sign out</Button>
-            ) : (
-              <Button onClick={handleGitHubLogin}>Sign in</Button>
-            )}
-          </div>
+          <Link
+            to="/app"
+            className="flex items-center mr-4 text-sm text-gray-300 transition-colors duration-150 cursor-pointer hover:text-white"
+          >
+            Go to app
+            <BiLinkExternal size={14} className="ml-2" />
+          </Link>
           <div className="flex items-center pl-4 space-x-5 border-l border-neutral-700">
             <a
               href="https://github.com/pheralb/docskit"
